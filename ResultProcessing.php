52420b2c806d3c995ab8b8f33215a93408d4a694
kensyuu1 <57424541+kensyuu1@users.noreply.github.com>
2020-12-08 10:59:43 +0900

    クイズ結果出力（仮）
    必要に応じて中身を変更する

---
+++ ResultProcessing.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>クイズ結果出力（仮）</title>
+</head>
+<body>
+<?php
+$seikaisuu = rand(0, 10);
+
+echo "あなたの正解数は「 $seikaisuu 」です。<br />";
+
+
+
+if ($seikaisuu < 3){
+    echo "もう少し頑張りましょう";
+}elseif ($seikaisuu < 9){
+    echo "まずまずの成果です";
+}elseif ($seikaisuu < 11){
+    echo "すばらしい";
+}
+
+
+?>
+
+</body>
+</html>
