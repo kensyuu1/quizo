89c1d0c85400610420f0889391c37954e392b68b
kensyuu1 <57424541+kensyuu1@users.noreply.github.com>
2020-12-15 09:44:11 +0900

    変更

--- ResultProcessing.php
+++ ResultProcessing.php
@@ -1,27 +1,69 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>クイズ結果出力（仮）</title>
+<title>ランダムメッセージ</title>
 </head>
 <body>
-    
-<?php
+
+<h1>リザルト画面</h1>
+    <button type="button" style="WIDTH: 170px; HEIGHT: 50px ;  position:absolute; top:600px; left:370px">
+        <font size="2">違うジャンルでもう一度</font>
+    </button>
+
+    <button type="button" style="WIDTH: 170px; HEIGHT: 50px;	position:absolute; top:600px; left:560px">
+
+        <font size="2">もう一度同じ問題</font>
+    </button>
+
+    <button type="button" style="WIDTH: 170px; HEIGHT: 50px;	position:absolute; top:600px; left:750px">
+
+        <font size="2">終わる</font>
+    </button>
+    <div style=" WIDTH: 300px; HEIGHT: 50px;    padding: 0.5em 1em;
+    margin: 2em 0;
+    font-weight: bold;
+    border: solid 3px #000000;
+    position:absolute; top:150px; left:250px">
+        <?php
 $seikaisuu = rand(0, 10);
 
-echo "あなたの正解数は「 $seikaisuu 」です。<br />";
+echo "あなたの正解数は";
+echo "$seikaisuu";
+echo "/10です";
 
 
-
-if ($seikaisuu < 3){
-    echo "もう少し頑張りましょう";
-}elseif ($seikaisuu < 9){
-    echo "まずまずの成果です";
-}elseif ($seikaisuu < 11){
-    echo "すばらしい";
-}
 
 
 ?>
+    </div>
+
+    <div style=" WIDTH: 500px; HEIGHT: 50px;     padding: 0.5em 1em;
+    margin: 2em 0;
+    font-weight: bold;
+    border: solid 3px #000000;
+    position:absolute; top:250px; left:170px">
+    <?php
+        echo "おすすめ情報";
+        if ($seikaisuu < 3){
+            echo "　　　もう少し頑張りましょう";
+        }elseif ($seikaisuu < 9){
+            echo "　　　まずまずの成果です";
+        }elseif ($seikaisuu < 11){
+            echo "　　　すばらしい";
+        }
+        
+        
+        ?>
+    </div>
+
+    <div style="z-index: -2; WIDTH: 800px; HEIGHT: 500px;     padding: 0.5em 1em;
+    margin: 2em 0;
+    font-weight: bold;
+    border: solid 3px #000000;
+    position:absolute; top:120px; left:120px">
+
+    </div>
+
 
 </body>
 </html>
